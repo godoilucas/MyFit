40fb0c5bed5b73b00e044cc0dee2aa9cd2581651
godoilucas <godoi.lucas@hotmail.com>
2020-11-22 14:32:05 -0300

    Add Lista Pessoas

--- backend/api/controllers/PessoaController.js
+++ backend/api/controllers/PessoaController.js
@@ -4,7 +4,9 @@ const utils = require("./Utilidades");
 class PessoaController{
     async listaPessoas(req, res){
         try {
+            const pessoas = await database.Pessoas.findAll();
             
+            return res.status(200).json(pessoas);
         } catch (error) {
             return res.status(500).json(error.message);
         }
